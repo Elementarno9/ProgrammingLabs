508d015f6fcfd0c6fc2daa820a00c5b4f9d22132
Elementarno <edwardpro9@gmail.com>
2020-03-27 14:00:34 +0300

    Update Lab #3 & #5

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -24,6 +24,7 @@ namespace Lab3
             return result;
         }
 
+        /*
         static double[] ReadFloatsFromConsole()
         {
             Console.Write("Введите размер массива: ");
@@ -33,6 +34,20 @@ namespace Lab3
             for (int i = 0; i < n; i++)
             {
                 arr[i] = ReadDouble($"[{i+1}] Введите число: ");
+            }
+
+            return arr;
+        }*/
+
+        static double[] ReadFloatsFromConsole()
+        {
+            Console.Write("Введите массив: ");
+            string[] nums = Console.ReadLine().Split();
+
+            var arr = new double[nums.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                double.TryParse(nums[i], out arr[i]);
             }
 
             return arr;
@@ -58,13 +73,22 @@ namespace Lab3
             // Sum between first and last negative numbers
             if (lastNegIdx > firstNegIdx)
             {
-                for (int i = firstNegIdx + 1; i < lastNegIdx; i++) { betweenNegSum += arr[i]; Console.WriteLine(i); }
+                for (int i = firstNegIdx + 1; i < lastNegIdx; i++) betweenNegSum += arr[i];
             }
 
             // Transform array
+            double[] resultArr = new double[arr.Length];
+            for (int i = 0, j = 0; i < arr.Length; i++)
+            {
+                if (arr[i] <= 1)
+                {
+                    resultArr[j] = arr[i];
+                    j++;
+                }
+            }
 
 
-            Console.WriteLine("Сумма нечетных элементов: {0}\nСумма между первым и последним отрицательным элементом: {1}", oddSum, betweenNegSum);
+            Console.WriteLine("Сумма нечетных элементов: {0}\nСумма между первым и последним отрицательным элементом: {1}\nОбновленный массив: {2}", oddSum, betweenNegSum, string.Join(" ", resultArr));
         }
     }
 }
