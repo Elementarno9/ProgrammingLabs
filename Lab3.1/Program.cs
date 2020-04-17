2edae24c3d50b7a9cec5e6b5cfe913df565546f8
Elementarno <edwardpro9@gmail.com>
2020-04-17 21:31:00 +0300

    Fix lab #3.1

--- Lab3.1/Program.cs
+++ Lab3.1/Program.cs
@@ -13,63 +13,54 @@ namespace Lab3._1
             ComputeFloats(matrix);
         }
 
-        static double[][] ReadMatrixFromConsole()
+        static double[,] ReadMatrixFromConsole()
         {
             Console.WriteLine("Ввод матрицы. (конец = пустая строка)");
-            var rows = new List<double[]>();
-            int maxLength = 0;
-            while (true)
-            {
-                Console.Write("[{0}]: ", rows.Count + 1);
-                string s = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(s)) break;
-                string[] nums = s.Split();
+            uint m = ReadNaturalInt("Кол-во строк: "), n = ReadNaturalInt("Кол-во столбцов: ");
+            var matrix = new double[n,m];
+            Console.WriteLine("{0} {1}", matrix.GetLength(0), matrix.GetLength(1));
 
-                if (nums.Length > maxLength) maxLength = nums.Length;
-                var arr = new double[nums.Length];
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    double.TryParse(nums[i], out arr[i]);
-                }
-                rows.Add(arr);
-
-            }
-            double[][] result = new double[rows.Count][];
-            for (int i = 0; i < rows.Count; i++)
+            for (int j = 0; j < m; j++)
             {
-                result[i] = new double[maxLength];
-                for (int j = 0; j < rows[i].Length; j++) result[i][j] = rows[i][j];
+                Console.Write("[{0}]: ",j + 1);
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input)) break;
+                string[] nums = input.Split();
+                for (int i = 0; i < Math.Min(n, nums.Length); i++) double.TryParse(nums[i], out matrix[i, j]);
             }
-            return result;
+
+            return matrix;
         }
 
-        static void ComputeFloats(double[][] matrix)
+        static void ComputeFloats(double[,] matrix)
         {
             Console.WriteLine("Суммы по строкам с неотрицательными числами:");
             double sum = 0;
-            for (int i = 0; i < matrix.Length; i++)
+            for (int i = 0; i < matrix.GetLength(1); i++)
             {
-                if (matrix[i].Min() >= 0)
+                if (MinInRow(matrix, i) >= 0)
                 {
-                    sum += matrix[i].Sum();
-                    Console.WriteLine("[{0}] Sum = {1}", i + 1, matrix[i].Sum());
+                    double rowsum = RowSum(matrix, i);
+                    sum += rowsum;
+                    Console.WriteLine("[{0}] Sum = {1}", i + 1, rowsum);
                 }
             }
             Console.WriteLine("Итого: {0}", sum);
 
             double minSum = double.MaxValue;
-            for (int i = 0; i < matrix[0].Length; i++)
+            for (int i = 0; i < Math.Max(matrix.GetLength(0), matrix.GetLength(1)); i++)
             {
                 var diags = new List<IEnumerable<double>>()
                 {
                     GetDiagonal(matrix, i, 0),
                     GetDiagonal(matrix, 0, i),
                     GetDiagonal(matrix, i, 0, -1),
-                    GetDiagonal(matrix, 0, i, -1)
+                    GetDiagonal(matrix, 0, i, -1),
+                    GetDiagonal(matrix, matrix.GetUpperBound(0), i),
+                    GetDiagonal(matrix, matrix.GetUpperBound(0), i, -1)
                 };
                 diags.ForEach((diag) => {
                     var list = diag.ToList();
-                    Console.WriteLine(string.Join(" ", list));
                     double sum = list.Sum();
                     if (list.Count > 0 && sum < minSum) minSum = sum;
                 });
@@ -77,39 +68,85 @@ namespace Lab3._1
             Console.WriteLine("Минимальная сумма элементов диагонали: {0}", minSum);
         }
 
-        static void PrintMatrix(double[][] matrix)
+        static void PrintMatrix(double[,] matrix)
         {
             int len = MaxInMatrix(matrix).ToString().Length;
 
             Console.WriteLine();
-            for (int i = 0; i < matrix.Length; i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
                 Console.Write("|");
-                for (int j = 0; j < matrix[i].Length; j++) Console.Write(" {0, " + len + "} ", matrix[i][j]);
+                for (int i = 0; i < matrix.GetLength(0); i++) Console.Write(" {0, " + len + "} ", matrix[i, j]);
                 Console.Write("|\n");
             }
             Console.WriteLine();
         }
 
-        static double MaxInMatrix(double[][] matrix)
+        static double MaxInMatrix(double[,] matrix)
         {
             double max = double.MinValue;
-            foreach (var row in matrix)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                double curMax = row.Max();
-                max = curMax > max ? curMax : max;
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    max = matrix[i, j] > max ? matrix[i, j] : max;
+                }
             }
             return max;
         }
-
-        static IEnumerable<double> GetDiagonal(double[][] matrix, int x, int y, int move = 1)
+        /*
+        static double MaxInRow(double[,] matrix, int m)
         {
-            while (x < matrix[0].Length && y < matrix.Length && x >= 0 && y >= 0)
+            double max = double.MinValue;
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                yield return matrix[y][x];
+                max = matrix[i, m] > max ? matrix[i, m] : max;
+            }
+            return max;
+        }*/
+        static double MinInRow(double[,] matrix, int m)
+        {
+            double min = double.MaxValue;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                min = matrix[i, m] < min ? matrix[i, m] : min;
+            }
+            return min;
+        }
+
+        static double RowSum(double[,] matrix, int m)
+        {
+            double sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i, m];
+            }
+            return sum;
+        }
+
+        static IEnumerable<double> GetDiagonal(double[,] matrix, int x, int y, int move = 1)
+        {
+            while (x < matrix.GetLength(0) && y < matrix.GetLength(1) && x >= 0 && y >= 0)
+            {
+                yield return matrix[x,y];
                 x += move;
                 y += 1;
             }
+        }
+
+        static uint ReadNaturalInt(string inputText)
+        {
+            bool flag = false;
+            uint result;
+            do
+            {
+                Console.Write(inputText);
+                string line = Console.ReadLine();
+                if (uint.TryParse(line, out result) && result != 0) flag = true;
+                else Console.WriteLine("Ошибка: нужно ввести натуральное число.");
+            } while (!flag);
+
+            return result;
         }
     }
 }
